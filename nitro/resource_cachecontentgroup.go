64a37c1c498407748592f7912eae361046d7af91
Charles-Edouard BRETECHE <ce.breteche@seloger.com>
2018-09-06 10:18:23 +0200

    re generated code

--- nitro/resource_cachecontentgroup.go
+++ nitro/resource_cachecontentgroup.go
@@ -29,7 +29,7 @@ type Cachecontentgroup struct {
 	Matchcookies           string   `json:"matchcookies,omitempty"`
 	Maxressize             int      `json:"maxressize,string,omitempty"`
 	Memlimit               int      `json:"memlimit,string,omitempty"`
-	Minhits                int      `json:"minhits,string,omitempty"`
+	Minhits                int      `json:"minhits,omitempty"`
 	Minressize             int      `json:"minressize,string,omitempty"`
 	Name                   string   `json:"name,omitempty"`
 	Persistha              string   `json:"persistha,omitempty"`
